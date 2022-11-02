f05a492d93b8d7e430a4269325feccdb390e7492
xhjvpn <90988484+xhjvpn@users.noreply.github.com>
2022-11-03 02:33:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="img.baocun.cc";
+        url.hostname="kk.ci";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
