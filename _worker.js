336345503fb6a13b857246f1cdec748702fabd0e
xhjvpn <90988484+xhjvpn@users.noreply.github.com>
2022-11-03 02:34:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="kk.ci";
+        url.hostname="https://web.telegram.org";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
